f537c29fff59092a6fc0b04d9edd7b4f1fb67678
Tatsiana Tratsinnikava <Tatsiana_Tratsinnikava@epam.com>
2018-11-20 11:47:21 +0300

    Cube values were added

--- data-generation/scatter_squares.py
+++ data-generation/scatter_squares.py
@@ -1,13 +1,23 @@
 import matplotlib.pyplot as plt
 
-x_values = list(range(1, 1001))
-y_values = [x**2 for x in x_values]
+x_values_2 = list(range(1, 5000))
+y_values_2 = [x**2 for x in x_values_2]
 
-plt.scatter(x_values, y_values, c=x_values ,cmap=plt.cm.Blues, edgecolor='none', s=20)
+x_values_3 = list(range(1, 5000))
+y_values_3 = [x**3 for x in x_values_3]
 
-plt.title("Square Numbers", fontsize=24)
-plt.xlabel("Value", fontsize=14)
-plt.ylabel("Square of Value", fontsize=14)
+# plt.scatter(x_values_2, y_values_2, c='red', edgecolor='none', s=20)
+# plt.scatter(x_values_3, y_values_3, c='b' ,cmap=plt.cm.Blues, edgecolor='none', s=20)
 
-plt.tick_params(axis='both', which='major', labelsize=14)
+ax = plt.subplot();
+ax.plot(x_values_2, y_values_2)
+ax.plot(x_values_3, y_values_3)
+
+# plt.title("Square Numbers", fontsize=24)
+# ax.xlabel("Value", fontsize=14)
+# ax.ylabel("Square of Value", fontsize=14)
+
+# ax.tick_params(axis='both', which='major', labelsize=14)
+
+
 plt.show()
