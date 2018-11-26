113e1fa267761857ab41c5f799ab888101ab08a8
Tatsiana Tratsinnikava <Tatsiana_Tratsinnikava@epam.com>
2018-11-26 12:48:17 +0300

    2 Random cube diagram was added

---
+++ data-generation/die_visual.py
@@ -0,0 +1,34 @@
+from die import Die
+import pygal
+
+# Создание кубика D6.
+die_1 = Die()
+die_2 = Die()
+
+# Моделирование серии бросков с сохранением результатов в списке.
+results = []
+for roll_num in range(1000):
+	result = die_1.roll() + die_2.roll()
+	results.append(result)
+
+print(results)
+
+# Анализ результатов.
+frequencies = []
+max_result = die_1.num_sides + die_2.num_sides
+for value in range(1, max_result+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+
+print(frequencies)
+
+# Визуализация результатов.
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
