e1f8b821b0204b00be7acde96bcd47ef4ac77011
Tatsiana Tratsinnikava <Tatsiana_Tratsinnikava@epam.com>
2018-12-28 18:10:54 +0300

    Url params were added

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+	"""Домашняя страница приложения Learning Log"""
+	return render(request, 'Learning_logs/index.html')
+	
