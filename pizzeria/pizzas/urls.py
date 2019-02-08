ed9b98a5830731de9b70a57dc5f958123a58dfcf
Tatsiana Tratsinnikava <Tatsiana_Tratsinnikava@epam.com>
2019-02-08 18:05:28 +0300

    Add and Edit forms were added

--- pizzeria/pizzas/urls.py
+++ pizzeria/pizzas/urls.py
@@ -12,5 +12,15 @@ urlpatterns = [
 	url(r'^pizzas/$', views.pizzas, name='list'),
 
 	# Страница с подробной информацией по ингредиенитам
-	url(r'^pizzas/(?P<pizza_id>\d+)/$', views.topping, name='topping')
+	url(r'^pizzas/(?P<pizza_id>\d+)/$', views.topping, name='topping'),
+
+	# Страница для добавления новой темы.
+	url(r'^new_pizza/$', views.new_pizza, name='new_pizza'),
+
+	# Страница для добавления новых игнредиентов.
+	url(r'^new_topping/(?P<pizza_id>\d+)/$', views.new_topping, name='new_topping'),
+
+	# Страница для редактирования игнредиентов.
+    url(r'^edit_topping/(?P<topping_id>\d+)/$', views.edit_topping, name='edit_topping'),
+   
 ]
--- pizzeria/pizzas/views.py
+++ pizzeria/pizzas/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Pizza, Topping
 
-# Create your views here.
+from django.http import HttpResponseRedirect
+# from django.core.urlresolvers import reverse
+from django.urls import reverse
+
+from .models import Pizza, Topping
+from .forms import PizzaForm, ToppingForm
 
 def index(request):
 	""" Домашняя страница приложения """
@@ -21,3 +26,129 @@ def topping(request, pizza_id):
 	context = {'pizza': pizza, 'toppings': toppings}
 
 	return render(request, 'pizzas/topping.html', context)
+
+def new_pizza(request):
+	"""Добавление новой пиццы."""
+	if request.method != 'POST':
+		# Данные не отправлялись; создается пустая форма.
+		form = PizzaForm()
+	else:
+		# Отправлены данные POST; обработать данные.
+		form = PizzaForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('pizzas:list'))
+
+	context = {'form': form}
+	return render(request, 'pizzas/new_pizza.html', context)
+
+def new_topping(request, pizza_id):
+	"""Добавление новых ингредиентов"""
+	pizza = Pizza.objects.get(id=pizza_id)
+	
+	if request.method != 'POST':
+		# Данные не отправлялись; создается пустая форма.
+		form = ToppingForm()
+	else:
+		# Отправлены данные POST; обработать данные.
+		form = ToppingForm(data=request.POST)
+		if form.is_valid():
+			new_topping = form.save(commit=False)
+			new_topping.pizza = pizza
+			new_topping.save()
+			return HttpResponseRedirect(reverse('pizzas:topping', args=[pizza_id]))
+
+	context = {'pizza': pizza, 'form': form }
+	return render(request, 'pizzas/new_topping.html', context)
+
+
+def edit_topping(request, topping_id):
+	"""Редактирование ингредиентов"""
+	topping = Topping.objects.get(id=topping_id)
+	pizza = topping.pizza
+
+	if request.method != 'POST':
+		# Данные не отправлялись; создается пустая форма.
+		form = ToppingForm(instance=topping)
+	else:
+		# Отправка данных POST; обработать данные.
+		form = ToppingForm(instance=topping, data=request.POST)
+		if form.is_valid():
+			form.save()
+			return HttpResponseRedirect(reverse('pizzas:topping', args=[topping.id]))
+
+	context = {'pizza': pizza, 'topping': topping, 'form': form }
+	return render(request, 'pizzas/edit_topping.html', context)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
